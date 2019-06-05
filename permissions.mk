d034e1620bab76b612a9e0b2f48440c23ea56dc6
Mr <mickaelmendes50@gmail.com>
2019-06-04 21:04:30 -0300

    permissions: Sort and order
    Signed-off-by: Mr <mickaelmendes50@gmail.com>

---
+++ permissions.mk
@@ -0,0 +1,145 @@
+# Acer Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/acer/com.acer.android.os.xml:system/etc/permissions/com.acer.android.os.xml
+
+# Asus Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/asus/asus.software.marketapp.xml:system/etc/permissions/asus.software.marketapp.xml \
+        $(LOCAL_PATH)/permissions/asus/asus.software.zenui.xml:system/etc/permissions/asus.software.zenui.xml
+
+# Blackberry Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/blackberry/com.blackberry.only.xml:system/etc/permissions/com.blackberry.only.xml
+
+# BQ Aquaris Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/bq/bq.software.bqservices.system.xml:system/etc/permissions/bq.software.bqservices.system.xml \
+	$(LOCAL_PATH)/permissions/bq/bq.software.qcom.camera.full_support.xml:system/etc/permissions/bq.software.qcom.camera.full_support.xml \
+	$(LOCAL_PATH)/permissions/bq/bq.software.qcom.camera4.xml:system/etc/permissions/bq.software.qcom.camera4.xml \
+	$(LOCAL_PATH)/permissions/bq/bq.software.qcom.m.bardock.xml:system/etc/permissions/bq.software.qcom.m.bardock.xml \
+	$(LOCAL_PATH)/permissions/bq/bq.software.qcom.m.xml:system/etc/permissions/bq.software.qcom.m.xml \
+	$(LOCAL_PATH)/permissions/bq/bq.software.qcom.n.bardock.xml:system/etc/permissions/bq.software.qcom.n.bardock.xml \
+	$(LOCAL_PATH)/permissions/bq/bq.software.qcom.n.xml:system/etc/permissions/bq.software.qcom.n.xml
+
+# Cyanogen Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/cyanogen/com.cyngn.crispybacon.xml:system/etc/permissions/com.cyngn.crispybacon.xml \
+        $(LOCAL_PATH)/permissions/cyanogen/com.cyngn.updater.xml:system/etc/permissions/com.cyngn.updater.xml
+
+# Essential Phone Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/essential/com.essential.feature.essential_experience.xml:system/etc/permissions/com.essential.feature.essential_experience.xml \
+	$(LOCAL_PATH)/permissions/essential/com.essential.score.lib.xml:system/etc/permissions/com.essential.score.lib.xml
+
+# Generic Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/generic/android.hardware.camera.ar.xml:system/etc/permissions/android.hardware.camera.ar.xml \
+	$(LOCAL_PATH)/permissions/generic/android.hardware.gamepad.xml:system/etc/permissions/android.hardware.gamepad.xml \
+	$(LOCAL_PATH)/permissions/generic/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
+
+# Google Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/google/pixel_2016_exclusive.xml:system/etc/permissions/pixel_2016_exclusive.xml \
+	$(LOCAL_PATH)/permissions/google/pixel_2017_exclusive.xml:system/etc/permissions/pixel_2017_exclusive.xml \
+	$(LOCAL_PATH)/permissions/google/pixel_2017.xml:system/etc/permissions/pixel_2017.xml \
+	$(LOCAL_PATH)/permissions/google/pixel_2018_exclusive.xml:system/etc/permissions/pixel_2018_exclusive.xml \
+	$(LOCAL_PATH)/permissions/google/pixel_2019_midyear_exclusive.xml:system/etc/permissions/pixel_2019_midyear_exclusive.xml \
+	$(LOCAL_PATH)/permissions/google/pixel_experience_2019_midyear.xml:system/etc/permissions/pixel_experience_2019_midyear.xml \
+	$(LOCAL_PATH)/permissions/google/pixel_experience_2018.xml:system/etc/permissions/pixel_experience_2018.xml \
+	$(LOCAL_PATH)/permissions/google/nexus.xml:system/etc/permissions/nexus.xml \
+	$(LOCAL_PATH)/permissions/google/google_build.xml:system/etc/permissions/google_build.xml \
+	$(LOCAL_PATH)/permissions/google/com.google.android.dialer.support.xml:system/etc/permissions/com.google.android.dialer.support.xml
+
+# HTC Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/htc/com.htc.htcadaptivesound_c.xml:system/etc/permissions/com.htc.htcadaptivesound_c.xml \
+	$(LOCAL_PATH)/permissions/htc/com.htc.key.dap.xml:system/etc/permissions/com.htc.key.dap.xml \
+	$(LOCAL_PATH)/permissions/htc/com.htc.sensor.autocalibration.xml:system/etc/permissions/com.htc.sensor.autocalibration.xml \
+	$(LOCAL_PATH)/permissions/htc/com.htc.sensor.edgesense.xml:system/etc/permissions/com.htc.sensor.edgesense.xml \
+	$(LOCAL_PATH)/permissions/htc/com.htc.sensor.hallsensor.xml:system/etc/permissions/com.htc.sensor.hallsensor.xml \
+	$(LOCAL_PATH)/permissions/htc/com.htc.sensor.sensorhub.xml:system/etc/permissions/com.htc.sensor.sensorhub.xml \
+	$(LOCAL_PATH)/permissions/htc/com.htc.software.market_ocean.xml:system/etc/permissions/com.htc.software.market_ocean.xml \
+	$(LOCAL_PATH)/permissions/htc/com.htc.voicedictation_c.xml:system/etc/permissions/com.htc.voicedictation_c.xml
+
+# Motorola Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.actions.perdefs.2017.xml:system/etc/permissions/com.motorola.actions.perdefs.2017.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.actions.perdefs.xml:system/etc/permissions/com.motorola.actions.perdefs.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.android.settings.shared.xml:system/etc/permissions/com.motorola.android.settings.shared.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.android.subsidyutils.xml:system/etc/permissions/com.motorola.android.subsidyutils.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.aov.xml:system/etc/permissions/com.motorola.aov.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.arselfie.xml:system/etc/permissions/com.motorola.arselfie.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.bettertogether.xml:system/etc/permissions/com.motorola.bettertogether.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.camera.xml:system/etc/permissions/com.motorola.camera.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.camera2.xml:system/etc/permissions/com.motorola.camera2.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.cameraone.xml:system/etc/permissions/com.motorola.cameraone.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.dtv.xml:system/etc/permissions/com.motorola.dtv.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.easyprefix.xml:system/etc/permissions/com.motorola.easyprefix.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.faceunlock.xml:system/etc/permissions/com.motorola.faceunlock.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.frameworks.core.addon.xml:system/etc/permissions/com.motorola.frameworks.core.addon.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.gallery.xml:system/etc/permissions/com.motorola.gallery.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.hardware.dtv.xml:system/etc/permissions/com.motorola.hardware.dtv.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.imager.evert.xml:system/etc/permissions/com.motorola.imager.evert.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.launcher3.gridcustomization.xml:system/etc/permissions/com.motorola.launcher3.gridcustomization.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.launcher3.twoline.xml:system/etc/permissions/com.motorola.launcher3.twoline.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.launcher3.xml:system/etc/permissions/com.motorola.launcher3.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.livestream.xml:system/etc/permissions/com.motorola.livestream.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.moto.xml:system/etc/permissions/com.motorola.moto.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.motoalexa.xml:system/etc/permissions/com.motorola.motoalexa.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.motodisplay.xml:system/etc/permissions/com.motorola.motodisplay.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.motokey.xml:system/etc/permissions/com.motorola.motokey.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.motosignature.xml:system/etc/permissions/com.motorola.motosignature.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.motosound.xml:system/etc/permissions/com.motorola.motosound.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.msimsettings.xml:system/etc/permissions/com.motorola.msimsettings.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.photoeditor.xml:system/etc/permissions/com.motorola.photoeditor.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.software.fmradioservice.xml:system/etc/permissions/com.motorola.software.fmradioservice.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.software.guideme.xml:system/etc/permissions/com.motorola.software.guideme.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.software.mya.xml:system/etc/permissions/com.motorola.software.mya.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.software.storageoptimizer.xml:system/etc/permissions/com.motorola.software.storageoptimizer.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.software.x_line.xml:system/etc/permissions/com.motorola.software.x_line.xml \
+	$(LOCAL_PATH)/permissions/motorola/com.motorola.timeweatherwidget.library.xml:system/etc/permissions/com.motorola.timeweatherwidget.library.xml \
+	$(LOCAL_PATH)/permissions/motorola/product_feature_evert.xml:system/etc/permissions/product_feature_evert.xml \
+	$(LOCAL_PATH)/permissions/motorola/product_feature_montana.xml:system/etc/permissions/product_feature_montana.xml \
+	$(LOCAL_PATH)/permissions/motorola/product_feature_nash.xml:system/etc/permissions/product_feature_nash.xml
+
+# Nextbit Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/nextbit/com.nextbit.camera.xml:system/etc/permissions/com.nextbit.camera.xml \
+        $(LOCAL_PATH)/permissions/nextbit/com.nextbit.constellation.xml:system/etc/permissions/com.nextbit.constellation.xml \
+        $(LOCAL_PATH)/permissions/nextbit/com.nextbit.gallery.xml:system/etc/permissions/com.nextbit.gallery.xml \
+        $(LOCAL_PATH)/permissions/nextbit/com.nextbit.launcher.xml:system/etc/permissions/com.nextbit.launcher.xml
+
+# Nokia Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/nokia/permissions/com.evenwell.datacollect.xml:system/etc/permissions/com.evenwell.datacollect.xml \
+        $(LOCAL_PATH)/permissions/nokia/permissions/com.fihtdc.datacollect.xml:system/etc/permissions/com.fihtdc.datacollect.xml \
+        $(LOCAL_PATH)/permissions/nokia/permissions/com.fihtdc.hardware.sensor.hall.xml:system/etc/permissions/com.fihtdc.hardware.sensor.hall.xml \
+        $(LOCAL_PATH)/permissions/nokia/permissions/com.fihtdc.inlifeui.settings.style.android.xml:system/etc/permissions/com.fihtdc.inlifeui.settings.style.android.xml
+
+# NVidia Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/nvidia/com.nvidia.diagtools.exposed.xml:system/etc/permissions/com.nvidia.diagtools.exposed.xml \
+	$(LOCAL_PATH)/permissions/nvidia/com.nvidia.feature.opengl4.xml:system/etc/permissions/com.nvidia.feature.opengl4.xml \
+	$(LOCAL_PATH)/permissions/nvidia/com.nvidia.feature.xml:system/etc/permissions/com.nvidia.feature.xml \
+	$(LOCAL_PATH)/permissions/nvidia/com.nvidia.graphics.xml:system/etc/permissions/com.nvidia.graphics.xml \
+	$(LOCAL_PATH)/permissions/nvidia/com.nvidia.miracast.xml:system/etc/permissions/com.nvidia.miracast.xml \
+	$(LOCAL_PATH)/permissions/nvidia/com.nvidia.nvsi.xml:system/etc/permissions/com.nvidia.nvsi.xml \
+	$(LOCAL_PATH)/permissions/nvidia/com.nvidia.shieldtech.xml:system/etc/permissions/com.nvidia.shieldtech.xml
+
+# OnePlus Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/oneplus/com.oneplus.software.xml:system/etc/permissions/com.oneplus.software.xml
+
+# Razerzone Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/razerzone/com.razerzone.features.xml:system/etc/permissions/com.razerzone.features.xml \
+	$(LOCAL_PATH)/permissions/razerzone/com.razerzone.features_aura.xml:system/etc/permissions/com.razerzone.features_aura.xml
+
+# Samsung Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/samsung/com.samsung.android.api.version.xml:system/etc/permissions/com.samsung.android.api.version.xml \
+	$(LOCAL_PATH)/permissions/samsung/com.samsung.device_subsfix.xml:system/etc/permissions/com.samsung.device_subsfix.xml
+
+# Sony Permissions
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/permissions/sony/com.sony.device.xml:system/etc/permissions/com.sony.device.xml
